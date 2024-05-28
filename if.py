8538bea17c4281aef40657aefc5a8e18f15bc6b8
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-05-28 19:50:18 +0200

    bomboclat

---
+++ if.py
@@ -0,0 +1,10 @@
+import operator
+from enum import Enum
+
+print("insert your first part of condition")
+firstCondition = input()
+print("insert your second part of condition")
+secondCondition = input()
+print("choose your comparison")
+
+
---
+++ start.py
@@ -0,0 +1,6 @@
+#Start
+print("hello mother fucker, what's your name")
+name = input()
+print("Hello", name, ",you fucking cunt")
+
+# ** for exponents, // di un divisione mi da l'integer e non la virgola (omega assurdo) 64 // 10 = 6 e non 6.4
